3e173633758d0c9ec655ce9dedd5e45befb54967
Parth Doshi <pdoshi287@gmail.com>
2020-04-16 12:35:31 +0530

    Linked List

--- linked_list.py
+++ linked_list.py
@@ -7,11 +7,25 @@ class LinkedList:
     def __init__(self):
         self.head = None
     
+    # Used for ptinting the list as well as for generating the length of the list
     def print_list(self):
         cur_node = self.head
         while cur_node:
             print(cur_node.data)
             cur_node = cur_node.next
+        
+    def length_iterative(self):
+        cur_node = self.head
+        count = 0
+        while cur_node:
+            cur_node = cur_node.next
+            count +=1
+        return count 
+
+    def length_reccursive(self, node):
+        if node is None:
+            return 0 
+        return 1 + self.length_reccursive(node.next)
 
     def append(self,data):
         new_node = Node(data)
@@ -36,12 +50,60 @@ class LinkedList:
             new_node.next = prev_node.next
             prev_node.next = new_node
 
-llist = LinkedList()
-llist.append("A")
-llist.append("B")
-llist.append("C")
-# llist.print_list()
-llist.prepend("E")
-print(llist.head.next.data)
-# llist.insert_after(llist.head.next, "F")
-# llist.print_list()
+    def delete_node(self,key):
+        #Case 1 : When the node to be deleted is the head node
+        cur_node = self.head
+        if cur_node and cur_node == key:
+            self.head = cur_node.next
+            cur_node = None
+            return
+
+        # Case 2 : When the node is not at the head
+        prev = None 
+        while cur_node and cur_node.data != key:
+            prev = cur_node
+            cur_node = cur_node.next
+        if cur_node is None:
+            return
+        prev.next = cur_node.next
+        cur_node = None
+    
+    def delete_node_at_position(self,pos):
+        cur_node = self.head
+        
+        if pos == 0:
+            self.head = cur_node.next
+            cur_node = None
+            return        
+        
+        count = 0
+        prev = None
+        while cur_node and count!=pos:
+            prev = cur_node
+            cur_node = cur_node.next
+            count +=1
+            
+        if cur_node is None:
+            return
+        
+        prev.next = cur_node.next
+        cur_node = None
+
+# Testing the functions out here
+if __name__ == "__main__":   
+    llist = LinkedList()
+    llist.append("A")
+    llist.append("B")
+    llist.append("C")
+    llist.prepend("E")
+    llist.insert_after(llist.head.next, "F")
+    print("\nOld List")
+    llist.print_list()
+    # print(llist.length_iterative())
+    print(llist.length_reccursive(llist.head))
+    # llist.delete_node("F")
+    llist.delete_node_at_position(2)
+    print("\nNew List")
+    llist.print_list()
+    print(llist.length_reccursive(llist.head))
+    # print(llist.length_iterative())
